a15c4cea40ddfb8910b77e486a113328efc99c85
DEV ASHISH VATS <vatsdev.1998@gmail.com>
2018-09-27 23:39:47 +0530

    codePy
    codePy

---
+++ SimplePyramid.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class SimplePyramid{
+public static void main(String args[]){
+int i;int j;
+for(i=0;i<5;i++){
+for(j=0;j<=i;j++){
+System.out.println("*");
+}
+System.out.println();
+}
+
+}
+}
